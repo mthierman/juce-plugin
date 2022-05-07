c756f3241395f560c40de8a5d9861bffc3daa107
Mike Thierman <mthierman@gmail.com>
2022-05-07 17:26:53 -0600

    ASIO PS module

---
+++ scripts/ASIO/ASIO.psm1
@@ -0,0 +1,31 @@
+$ASIO_SDK_URL="https://www.steinberg.net/asiosdk"
+$SDK_DOWNLOAD_FILE="asiosdk.zip"
+$LIBS_DIR="libs"
+$SDK_DIRNAME="asio"
+$SDK_LOCATION="$LIBS_DIR\$SDK_DIRNAME"
+
+function Get-ASIOSDK {
+  Write-Output "Checking for Steinberg ASIO SDK"
+  if (! (Test-Path $SDK_LOCATION)) {
+    if (! (Test-Path $SDK_DOWNLOAD_FILE)) {
+      Write-Output "Downloading $SDK_DOWNLOAD_FILE"
+      Invoke-WebRequest -Uri $ASIO_SDK_URL -UseBasicParsing -OutFile $SDK_DOWNLOAD_FILE
+    }
+    else {
+      Write-Output "Found $SDK_DOWNLOAD_FILE"
+    }
+    Write-Output "Extracting to $SDK_LOCATION"
+    Expand-Archive $SDK_DOWNLOAD_FILE $LIBS_DIR
+    Push-Location -Path $LIBS_DIR
+    (Get-ChildItem -Filter "$SDK_DIRNAME*")[0] | Rename-Item -NewName { $_.Name -Replace $_.Name, $SDK_DIRNAME }
+    Pop-Location
+  }
+  else {
+    Write-Output "Steinberg ASIO SDK location: $SDK_LOCATION"
+  }
+  if ((Test-Path $SDK_DOWNLOAD_FILE)) {
+    Remove-Item $SDK_DOWNLOAD_FILE
+    Write-Output "Cleaning up $SDK_DOWNLOAD_FILE"
+  }
+}
+Set-Alias -Name asio -Value Get-ASIOSDK
